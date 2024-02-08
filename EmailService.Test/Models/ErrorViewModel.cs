7db8de030ab34b88791505d64126b6a51f70df5a
Prakhar <agrawalprakhar893@gmail.com>
2024-02-08 11:28:12 +0530

    Created common testing  for all services

---
+++ EmailService.Test/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EmailService.Test.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
